bc37f25612e89eb63e1bde421f9f6f8c1c382904
zzacharo <zacharias.zacharodimos@cern.ch>
2025-11-12 18:17:02 +0100

    fix: remove build related artifacts

--- run-tests.sh
+++ run-tests.sh
@@ -22,7 +22,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-python -m check_manifest
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch2} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
 python -m pytest
 tests_exit_code=$?
